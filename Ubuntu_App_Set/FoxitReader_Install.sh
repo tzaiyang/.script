14adc94dc3973e34cb1b24d28bcabd4b01ce2ade
tzaiyang <tzaiyang@github.com>
2018-01-27 11:37:33 +0800

    new file:   A33_Vstar_Development/Android_Compile.sh 	new file:   Ubuntu_App_Set/Chinese_Messy_Code_Fix.sh 	new file:   Ubuntu_App_Set/FoxitReader_Install.sh 	modified:   Ubuntu_App_Set/Over_GFW_In_SS.sh 	new file:   Ubuntu_App_Set/Uncompress_Compress_Command.sh 	modified:   Ubuntu_Development_Set/Opencv_Install.sh 	new file:   Ubuntu_Development_Set/Oracle_JDK_Install.sh 	new file:   Ubuntu_Development_Set/SSH_Use_OnGitHub.sh 	modified:   Ubuntu_Development_Set/Sublime_Text3_Chinese_Input_Set.sh 	modified:   Ubuntu_Development_Set/Tensorflow_Install.sh 	renamed:    iTop-4412-Development/madplay_download_to_itop-4412.sh -> iTop_4412_Development/madplay_download_to_itop-4412.sh 	renamed:    iTop-4412-Development/madplay_install_arm.sh -> iTop_4412_Development/madplay_install_arm.sh 	renamed:    iTop-4412-Development/madplay_install_x86.sh -> iTop_4412_Development/madplay_install_x86.sh

---
+++ Ubuntu_App_Set/FoxitReader_Install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader2.4.1.0609_Server_x64_enu_Setup.run.tar.gz
+gzip -d FoxitReader2.4.1.0609_Server_x64_enu_Setup.run.tar.gz
+tar xvf FoxitReader2.4.1.0609_Server_x64_enu_Setup.run.tar
+./FoxitReader.enu.setup.2.4.1.0609\(r08f07f8\).x64.run
--- Ubuntu_Development_Set/Tensorflow_Install.sh
+++ Ubuntu_Development_Set/Tensorflow_Install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 sudo apt-get install python-pip python-dev   # for Python 2.7
 pip install tensorflow      # Python 2.7; CPU support (no GPU support)
+
+#python 3.5
+#sudo pip3 install tensorflow
+
