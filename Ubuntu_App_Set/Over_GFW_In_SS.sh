7ba8c9e36d847a3fa8b6eaa902fa96e80620a0e2
iTOP-4412 <topeet@ubuntu.(none)>
2018-01-31 10:26:06 -0800

    new file:   Surport_Files/samba_share.conf 	new file:   Surport_Files/shadowsocks.json 	new file:   Surport_Files/supervisord.conf 	modified:   Ubuntu_App_Set/Over_GFW_In_SS.sh 	new file:   Ubuntu_App_Set/Ubuntu12_04_PreInstall.sh 	new file:   Ubuntu_App_Set/Ubuntu16_04_PreInstall.sh 	new file:   Ubuntu_Development_Set/Oracle_backup 	new file:   Ubuntu_Development_Set/Samba_Install.sh

--- Ubuntu_App_Set/Over_GFW_In_SS.sh
+++ Ubuntu_App_Set/Over_GFW_In_SS.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #服务器端
-
+#su - root
 
 #客户端
 #参考http://blog.csdn.net/u012810317/article/details/52139361
-sudo apt install shadowsocks
+sudo apt-get install shadowsocks
 
 #开机自启设置 
 #方案1
 sudo apt-get install supervisor
-sudo echo $(cat supervisor.conf) >> /etc/supervisor/supervisord.conf
+sudo echo $(cat ../Surport_Files/supervisord.conf) >> /etc/supervisor/supervisord.conf
 sudo service supervisor restart
 #在exit 0上一行加入以下内容：service supervisor start
 sed -i -e 's|exit\ 0|service\ supervisor\ start\nexit\ 0|' /etc/rc.local
@@ -17,11 +17,12 @@ sed -i -e 's|exit\ 0|service\ supervisor\ start\nexit\ 0|' /etc/rc.local
 #sed -i -e '/usr/bin/sslocal -c ~/.script/shadowsocks.json'  /etc/rc.local
 
 #网络代理改成Automatic:   Socks Host:127.0.0.1:1080
-pip install genpac
-genpac --proxy="SOCKS5 127.0.0.1:1080" -o autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+sudo apt-get install pip
+sudo pip install genpac
+genpac --proxy="SOCKS5 127.0.0.1:1080" -o ~/.script/Surport_Files/autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 #设置全局代理
 #点击：System settings > Network > Network Proxy ; 
 #选择 Method 为 Automatic 
-#设置 Configuration URL 为 autoproxy.pac 文件的路径(file:///home/ryan/autoproxy.pac)
+#设置 Configuration URL 为 autoproxy.pac 文件的路径(file:///home/ryan/.script/Surport_Files/autoproxy.pac)
 #点击 Apply System Wide。 
 #格式如：file:///home/{user}/vpnPAC/autoproxy.pac
---
+++ Ubuntu_App_Set/Ubuntu12_04_PreInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Open the terminal in current file path
+sudo apt-get install nautilus-open-terminal
+#log out and log in to active
+
+#Install VIM
+sudo apt-get install vim
+
+
+
