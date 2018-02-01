2a957891f497f899c817193728d62843085d3be7
tzaiyang <tzaiyang@github.com>
2018-02-02 01:25:26 +0800

    Added the part of server over GFW 	modified: App/Over_GFW_In_SS.sh

--- App/Over_GFW_In_SS.sh
+++ App/Over_GFW_In_SS.sh
@@ -1,12 +1,77 @@
 #!/bin/bash
 #服务器端
+#从DigitOcean买San Francisco的$5/mon的服务器,装CentOS6.9x64系统,创建成功后,root密码会发到账号邮箱.
+# tzaiyang@126.com账号下:root@104.236.178.238  120825@ryan
+# com.tzaiyang@gmail.com账号下:root@159.89.147.70 120825@ryan
+
+#su -root
+# wget --no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+# chmod +x shadowsocks.sh
+# ./shadowsocks.sh 2>&1 | tee shadowsocks.log
+# # 设置密码,端口号:如8989,加密方式:如AES-256-CFB
+
+# # 安装成功后显示如下:
+# # Congratulations, Shadowsocks-python server install completed!
+# # Your Server IP        :your_server_ip
+# # Your Server Port      :your_server_port
+# # Your Password         :your_password
+# # Your Encryption Method:your_encryption_method
+
+# # Welcome to visit:https://teddysun.com/342.html
+# # Enjoy it!
+
+# #unintall 
+# ./shadowsocks.sh uninstall
+
+# #单用户配置文件示例（2015 年 08 月 28 日修正）：
+# #配置文件路径：/etc/shadowsocks.json
+
+# {
+#     "server":"0.0.0.0",
+#     "server_port":your_server_port,
+#     "local_address":"127.0.0.1",
+#     "local_port":1080,
+#     "password":"your_password",
+#     "timeout":300,
+#     "method":"your_encryption_method",
+#     "fast_open": false
+# }
+
+# #多用户多端口配置文件示例（2015 年 08 月 28 日修正）：
+# #配置文件路径：/etc/shadowsocks.json
+
+# {
+#     "server":"0.0.0.0",
+#     "local_address":"127.0.0.1",
+#     "local_port":1080,
+#     "port_password":{
+#          "8989":"password0",
+#          "9001":"password1",
+#          "9002":"password2",
+#          "9003":"password3",
+#          "9004":"password4"
+#     },
+#     "timeout":300,
+#     "method":"your_encryption_method",
+#     "fast_open": false
+# }
+
+# #使用命令（2015 年 08 月 28 日修正）：
+# #启动：
+# /etc/init.d/shadowsocks start
+# #停止：
+# /etc/init.d/shadowsocks stop
+# #重启：
+# /etc/init.d/shadowsocks restart
+# #状态：
+# /etc/init.d/shadowsocks status
 
 
 
+#客户端Ubuntu16.04
 #su - root
 set -e
 
-#客户端
 #参考http://blog.csdn.net/u012810317/article/details/52139361
 sudo apt-get install shadowsocks
 sudo cp /etc/shadowsocks/config.json /etc/shadowsocks/config.json.bak
