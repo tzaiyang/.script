5f70b5e8270c98a401b2d5c7f383a71604097b66
tzaiyang <tzaiyang@github.com>
2018-02-01 20:26:15 +0800

    Rename All Diretory New Ubuntu16_04_PreInstall.sh Improved the Android4_4_Compile.sh

--- App/Chinese_Messy_Code_Fix_N.sh
+++ App/Chinese_Messy_Code_Fix_N.sh
@@ -1,3 +1,6 @@
 #unzip解压缩中文乱码解决方案
 unzip -O cp936 压缩文件名
 
+#查看文件编码方式
+file filename
+
--- App/Over_GFW_In_SS.sh
+++ App/Over_GFW_In_SS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #服务器端
 #su - root
+set -e
 
 #客户端
 #参考http://blog.csdn.net/u012810317/article/details/52139361
@@ -9,7 +10,7 @@ sudo apt-get install shadowsocks
 #开机自启设置 
 #方案1
 sudo apt-get install supervisor
-sudo echo $(cat ../Surport_Files/supervisord.conf) >> /etc/supervisor/supervisord.conf
+sudo cat ../Surport/supervisord.conf >> /etc/supervisor/supervisord.conf
 sudo service supervisor restart
 #在exit 0上一行加入以下内容：service supervisor start
 sed -i -e 's|exit\ 0|service\ supervisor\ start\nexit\ 0|' /etc/rc.local
@@ -17,12 +18,12 @@ sed -i -e 's|exit\ 0|service\ supervisor\ start\nexit\ 0|' /etc/rc.local
 #sed -i -e '/usr/bin/sslocal -c ~/.script/shadowsocks.json'  /etc/rc.local
 
 #网络代理改成Automatic:   Socks Host:127.0.0.1:1080
-sudo apt-get install pip
+sudo apt-get install python-pip python-dev build-essential
 sudo pip install genpac
-genpac --proxy="SOCKS5 127.0.0.1:1080" -o ~/.script/Surport_Files/autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+genpac --proxy="SOCKS5 127.0.0.1:1080" -o ../Surport/autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 #设置全局代理
 #点击：System settings > Network > Network Proxy ; 
 #选择 Method 为 Automatic 
-#设置 Configuration URL 为 autoproxy.pac 文件的路径(file:///home/ryan/.script/Surport_Files/autoproxy.pac)
+#设置 Configuration URL 为 autoproxy.pac 文件的路径(file:///home/ryan/.script/Surport/autoproxy.pac)
 #点击 Apply System Wide。 
 #格式如：file:///home/{user}/vpnPAC/autoproxy.pac
