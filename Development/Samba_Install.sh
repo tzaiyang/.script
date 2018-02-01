b3f79467c23574d944383d9146d0f349d8d2c8c9
iTOP-4412 <topeet@ubuntu.(none)>
2018-02-01 10:03:04 -0800

    Fixed Samba Install

--- Development/Samba_Install.sh
+++ Development/Samba_Install.sh
@@ -4,6 +4,6 @@
 sudo apt-get update
 sudo apt-get install samba
 
-sudo cat ../Surport_Files/samba_share.conf >> /etc/samba/smb.conf
+sudo cat ../Surport/samba_share.conf >> /etc/samba/smb.conf
 sudo service smbd restart
 
