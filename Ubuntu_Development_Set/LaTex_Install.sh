3316686cef863374d555951d59dc92aab20433ba
tzaiyang <tzaiyang@stumail.neu.edu.cn>
2018-02-01 23:27:50 +0800

    Improved LaTex Install

--- Ubuntu_Development_Set/LaTex_Install.sh
+++ Ubuntu_Development_Set/LaTex_Install.sh
@@ -5,3 +5,26 @@ sudo apt-get install libdigest-perl-md5-perl perl-tk
 sudo mount -o loop texlive2017.iso /mnt/
 cd /mnt
 sudo ./install-tl -gui
+
+#设置环境变量：
+#sudo gedit ~/.bashrc
+#打开之后在文本最后添加：
+
+echo 'export MANPATH=${MANPATH}:/usr/local/texlive/2017/texmf-dist/doc/man' >> .bashrc
+echo 'export INFOPATH=${INFOPATH}:/usr/local/texlive/2017/texmf-dist/doc/info' >> .bashrc
+echo 'export PATH=${PATH}:/usr/local/texlive/2017/bin/x86_64-linux' >> .bashrc
+
+source ~/.bashrc
+#卸载iso镜像：
+
+cd ..
+sudo umount /mnt/
+
+
+#配置sublime text 3
+#打开sublime text，ctrl + shift + p，输入install package，输入LaTeXtools，安装。
+#Preference - Package Settings - LaTeXtools - Settings user
+#将"texpath" : "$PATH:/usr/texbin",
+#改为：
+#"texpath" : "$PATH:/usr/local/texlive/2016/bin/x86_64-linux",
+#保存，退出。
