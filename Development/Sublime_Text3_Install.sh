5f8dc168c7a6dd7866e0df39060fb99cb2534b07
tzaiyang <tzaiyang@stumail.neu.edu.cn>
2018-02-05 13:51:06 +0800

    Update shadowsocks.json && Sublime_Text3 install 	new file:   App/Gnome_Install.sh 	modified:   App/Mac_Theme_Install.sh 	modified:   App/Mac_Theme_Uninstall.sh 	modified:   Development/Sublime_Text3_Install.sh 	modified:   Surport/shadowsocks.json

--- Development/Sublime_Text3_Install.sh
+++ Development/Sublime_Text3_Install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+#Install the GPG key:
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+#Ensure apt is set up to work with https sources:
+sudo apt-get install apt-transport-https
+#Select the channel to use:
+	#Stable
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+	#Dev
+	echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+#Update apt sources and install Sublime Text
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get install sublime-text
+
 
 
 #uninstall
-#sudo apt-get remove sublime-text-installer
+#sudo apt-get remove sublime-text
 
 #Chinese Input method Setting
 sudo apt-get update && sudo apt-get upgrade
