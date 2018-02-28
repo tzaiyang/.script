2faf9dab2a6b6c5acc58347c1018f4ddba545812
ansel <tzaiyang@126.com>
2018-02-27 23:22:11 -0800

    update A33 script(comment in english)
    	modified:   A33/ssh_install_on_A33.sh
    	renamed:    A33/Wifi_Config_on_A33.sh -> A33/wifi_config_on_A33.sh

--- A33/ssh_install_on_A33.sh
+++ A33/ssh_install_on_A33.sh
@@ -1,49 +1,44 @@
 #! /bin/sh
+#/dragonboard/bin/wifi_config_on_A33.sh
+# mkdir -p /usr/sbin
+mkdir -p /usr/local/bin
+mkdir -p /usr/local/libexec
+mkdir -p /usr/local/etc/
 
-#将ssh_compile.sh生成的usr目录递归拷贝到开发板根目录
+cp /dragonboard/usr/* -r /usr/
+# create soft link
+ln -s /usr/local/bin/scp /bin
+ln -s /usr/local/bin/sftp /bin
+ln -s /usr/local/bin/ssh /bin
+ln -s /usr/local/bin/ssh-add /bin
+ln -s /usr/local/bin/ssh-agent /bin
+ln -s /usr/local/bin/ssh-keygen /bin
+ln -s /usr/local/bin/ssh-keyscan /bin
 
-#调用Wifi_Config.sh连接wifi
-
-# 确保目标板上有以下目录，如果没有，则新建（目录层次很重要，不要改变）
-# /usr/sbin
-# /usr/local/bin
-# /usr/local/libexec
-# /usr/local/etc/
-
-#7、在开发板 bin 目录下创建软连接
-ln -s /usr/local/bin/scp 
-ln -s /usr/local/bin/sftp 
-ln -s /usr/local/bin/ssh 
-ln -s /usr/local/bin/ssh-add 
-ln -s /usr/local/bin/ssh-agent 
-ln -s /usr/local/bin/ssh-keygen 
-ln -s /usr/local/bin/ssh-keyscan
-#9、启动开发板，添加用户
-touch /etc/passwd 
-touch /etc/group
+# add user
+#touch /etc/passwd 
+#touch /etc/group
 mkdir -p /home/sshd
-adduser sshd
-adduser root
-# passwd root #为root用户设置密码
+#adduser sshd
+#adduser root
+passwd root #set root key
 
-#10、测试
-# 一开始提示密钥啥玩意，输入yes再输入密码就可以登录了，也可以直接用windows下的secure crt
-vi /usr/local/etc/sshd_config
-# 将开发板/usr/local/sshd_config,将PermitRootLogin yes前的注释“#”号去掉
-# 如出现如下错误
-# Permissions 0755 for '/usr/local/etc/ssh_host_rsa_key' are too open.
-# /var/empty must be owned by root and not group or world-writable
+# test 
+sed -i 's/#PermitRootLogin/PermitRootLogin/' /usr/local/etc/sshd_config
+
 # Solve method
 chmod 700 /usr/local/etc/ssh_host_ecdsa_key 
 chmod 700 /usr/local/etc/ssh_host_dsa_key 
 chmod 700 /usr/local/etc/ssh_host_rsa_key 
+mkdir -p /var/empty/
 chmod 744 /var/empty/  
+
 /usr/sbin/sshd 
-# ps 可以看到 /usr/sbin/sshd
 ps
 
-# 主机上登录测试: $  ssh root@192.168.1.182(要保证先ping通)
-# 如果出现问题：
+# test on linux host:$ssh root@192.168.1.218(ping)
+# if errors like follow :
+
 # ssh carlh at 192.168.52.190
 # carlh at 192.168.52.190's password: 
 # WARNING: Your password has expired.
@@ -56,8 +51,8 @@ ps
 # Connection to 192.168.52.190 closed.
 
 # Solve method:
-cat /etc/shadow
-#（和/etc/passwd是一一对应的)
+#cat /etc/shadow
+#（/etc/passwd and /etc/shadow are one by one)
 # root:[deleted field]:0:0:99999:7:::
 # bin:*:10933:0:99999:7:::
 # daemon:*:10933:0:99999:7:::
@@ -73,8 +68,7 @@ cat /etc/shadow
 # carlh:[deleted field]:0:0:99999:7:::
 
 # Solve Method:
-# 把: root:[deleted field]:0:0:99999:7::: 改为:# root:[deleted field]:10932:0:99999:7:::
+sed -i 's/:0:0:99999:7:::/:10933:0:99999:7:::/' /etc/shadow                                                                       
+#.......Test......
+#ssh root@192.168.3.218
 
-#主机再次登录
-ssh root@192.168.3.182
-#成功
--- A33/wifi_config_on_A33.sh
+++ A33/wifi_config_on_A33.sh
@@ -4,16 +4,16 @@ cd
 ln -s /dragonboard/bin/config.txt /system/vendor/modules/
 ln -s /dragonboard/bin/fw_bcm40181a2.bin /system/vendor/modules/
 ln -s /dragonboard/bin/nvram_ap6210.txt /system/vendor/modules/
-#启动wlan0
+# start wlan0
 ifconfig wlan0 up 
-#配置一个静态IP
-ifconfig wlan0 192.168.1.25
-#写入配置
+# config staic IP
+ifconfig wlan0 192.168.3.25
+# editting configure file 
 wpa_passphrase "AIWAC" "epic2012" > wifi.conf
-#启动配置
+# start wlan0
 wpa_supplicant -B -i wlan0 -Dwext -c ./wifi.conf
 iwconfig wlan0 
-#Setting gateway
-route add default gw 192.168.1.201
-#dhcp自动分配IP
+# setting gateway
+route add default gw 192.168.3.201
+# dhcp
 udhcpc -iwlan0
