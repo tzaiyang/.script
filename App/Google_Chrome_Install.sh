d923edf60f890f72d4782c9108b912a94fa0a76b
ansel@tang-H81M-DS2-D <tzaiyang@github.com>
2018-04-14 16:37:42 +0800

    modified:   App/Google_Chrome_Install.sh 	new file:   Development/GPU_Server_Docker_Config.sh 	new file:   Development/GPU_Server_LXC_Config.sh

--- App/Google_Chrome_Install.sh
+++ App/Google_Chrome_Install.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-set -e
-# HOME_DIR=$PWD
-
-# wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# cd /tmp
-
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
-# sudo apt-get -f install
-
-# rm *.deb
-# cd HOME_DIR
-
-
-#通过 PPA 源安装
-#首先安装密钥
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-#添加 PPA 源
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt-get install google-chrome-stable
-#sudo apt-get install google-chrome-beta
-
+. Surport/Default_Varible.sh
+wget -P $DownloadAddress https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i $DownloadAddress/google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
