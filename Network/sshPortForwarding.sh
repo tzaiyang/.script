a85a49fe66e9dee6c64c5afcda5a584c320dd959
tzaiyang <tzaiyang@126.com>
2019-01-17 10:33:53 +0800

    add sshPortForwarding.sh to rc.local and fixed the path bug

--- Network/sshPortForwarding.sh
+++ Network/sshPortForwarding.sh
@@ -5,8 +5,10 @@
 serverUser=admin
 serverIP=yourip
 ForwardingPort=7002
-rsa=~/.ssh/id_rsa
+rsa=/home/username/.ssh/id_rsa
 exit=1
+
+# ssh-copy-id -i /home/username/.ssh/id_rsa.pub ${serverUser}@${serverIP}
 
 while test $exit -eq 1
 do
@@ -28,5 +30,18 @@ done
 # # server host
 # tragetUser = admin
 # ssh -p $(ForwardingPort) $(tragetUser)@localhost
+
+############## add rc.local######################
+
+#!/bin/bash -e 
+
+# #log
+# exec 2 > /tmp/rc.local.log
+# exec 1>&2 
+# set -x 
+
+# xxx/sshPortForwarding.sh
+
+# exit 0
 
 
